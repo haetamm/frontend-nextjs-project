b86c9b8b8a9ae5017721103266a408ffecc1ea2f
Thaetami <tatanghaetami.97@gmail.com>
2023-09-05 19:10:13 +0700

    fixing cors #4 [deployment]

--- utils/handleLogout.js
+++ utils/handleLogout.js
@@ -3,6 +3,7 @@ import endpoint from '../utils/api-endpoint';
 const handleLogout = async (router, id) => {
     try {
       const response = await endpoint.delete(`auth/${id}`);
+       document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
       console.log(response)
       // router.reload();
     } catch (error) {
