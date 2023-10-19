09ba0e296ce733b85c6d6d86d0ccc6dc9d852980
Thaetami <tatanghaetami.97@gmail.com>
2023-10-20 06:55:40 +0700

    trying update #deployment_process

--- utils/handleLogout.js
+++ utils/handleLogout.js
@@ -3,7 +3,9 @@ import endpoint from '../utils/api-endpoint';
 const handleLogout = async (router, id) => {
     try {
       const response = await endpoint.delete(`auth/${id}`);
-       document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      //  document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=None; Secure';
+
       // console.log(response)
       console.log(document.cookie);
       // router.reload();
