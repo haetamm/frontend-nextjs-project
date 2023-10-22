8aeb63cfb84a890bc1b0634a631914186ea3cdf8
Thaetami <tatanghaetami.97@gmail.com>
2023-10-23 06:54:39 +0700

    add export path map configuration #deployment_process

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/admin': { page: '/admin' },
+      '/create': { page: '/create' },
+      '/guest/login': { page: '/guest/login' },
+      '/guest/register': { page: '/guest/register' },
+      '/profile': { page: '/profile' },
+      '/thread': { page: '/thread' },
+      '/update': { page: '/update' },
+      '/[...username]': { page: '/[...username]' },
+      '/[...username]/likes': { page: '/[...username]/likes' },
+      '/thread/[slug]': { page: '/thread/[slug]' },
+      '/update/[...threadSlug]': { page: '/update/[...threadSlug]' },
+    };
+  },
   images: {
     domains: [
       'cdn-icons-png.flaticon.com'
